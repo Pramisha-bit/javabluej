0ed238cbe9f166146408cf8820c4652f743f7196
Pramisha <pramishakarki582@gmail.com>
2025-11-21 13:08:26 +0545

    Initial sharing of project

---
+++ hello12.java
@@ -0,0 +1,62 @@
+
+/**
+ * Write a description of class hello12 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class hello12
+{
+    int arg;//instance variable
+    static int rtr;// static variable
+
+
+public static void main (String[] args);
+int age=18;//local variable= 32 bits
+
+    System.out.println(age);
+     
+    // <className> <varaible>=new <className>();
+    Hello12 B= new Hello12();
+    
+    
+    System.out.println(rtr);
+    System.out.println(B.args);
+    //Implict typecasting
+    double db=age;
+    
+    System.out.println(db);
+    
+    
+    
+    //Explicit typecasting
+    double dbr=10.90;
+    int itr= (int)dbr;
+    System.out.println(itr);
+    
+    
+    
+    //Exceptions
+    short st=10;
+    short str=11;
+    
+    short sum= (short)(st + str);
+    
+    
+    //Maximum, minimum, bits, bytes
+    
+    
+    
+    System.out.println(Byte.MAX_Value);
+    System.out.println(Byte.MIN_Value);
+    System.out.println(Byte.SIZE);
+    System.out.println(BYTES);
+    
+    
+    
+    
+    
+    
+    
+    
+}
